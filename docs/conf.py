8768261543c9bd7a14ffedbdf4d50f93bb3e1ee1
Mario Nugnes <mario.nugnes1@gmail.com>
2024-04-12 07:47:02 +0100

    moved directories

--- docs/conf.py
+++ docs/conf.py
@@ -19,9 +19,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../python_downloader'))
+sys.path.insert(0, os.path.abspath('../src/python_downloader'))
 
-import python_downloader
+from src import python_downloader
 
 # -- General configuration ---------------------------------------------
 
